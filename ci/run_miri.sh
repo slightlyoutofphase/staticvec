db5c99e16583a18d6d3bece7c4ee6b8db55fe9bf
slightlyoutofphase <slightlyoutofphase@gmail.com>
2019-11-25 19:48:55 -0500

    Add a more robust Miri script. Clearer function names in one of the benchmarks.

---
+++ ci/run_miri.sh
@@ -0,0 +1,6 @@
+MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
+echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
+rustup set profile minimal
+rustup default "$MIRI_NIGHTLY"
+rustup component add miri
+cargo miri test -j16 --features="std"
