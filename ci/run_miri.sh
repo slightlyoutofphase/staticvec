ae277a76d910e39399a7bdfdd1cf183cc176b344
slightlyoutofphase <slightlyoutofphase@gmail.com>
2019-12-07 11:29:30 -0500

    Restore Miri script now that it's working again, plus some other stuff

---
+++ ci/run_miri.sh
@@ -0,0 +1,7 @@
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri); fi
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-apple-darwin/miri); fi
+echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
+rustup set profile minimal
+rustup default "$MIRI_NIGHTLY"
+rustup component add miri
+yes | cargo miri test --features="std"
