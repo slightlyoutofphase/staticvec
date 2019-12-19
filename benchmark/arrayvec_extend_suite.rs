902ceac8122a8ddde6f230e3c01cdde458ea08fa
slightlyoutofphase <slightlyoutofphase@gmail.com>
2019-12-18 20:31:24 -0500

    Re-jig benchmarks a bit to get rid of dev-dependency that isn't really worth it at this point

--- benchmark/arrayvec_extend_suite.rs
+++ benchmark/arrayvec_extend_suite.rs
@@ -8,7 +8,6 @@ extern crate test;
 use std::io::Write;
 use test::{black_box, Bencher};
 
-use arrayvec::ArrayVec;
 use staticvec::StaticVec;
 
 #[bench]
@@ -24,33 +23,8 @@ fn staticvec_extend_from_slice(b: &mut Bencher) {
 }
 
 #[bench]
-fn arrayvec_extend_from_slice(b: &mut Bencher) {
-  let mut v = ArrayVec::<[u8; 512]>::new();
-  let data = [1; 512];
-  b.iter(|| {
-    v.clear();
-    v.try_extend_from_slice(black_box(&data[..])).ok();
-    v[511]
-  });
-  b.bytes = v.capacity() as u64;
-}
-
-#[bench]
 fn staticvec_extend_with_constant(b: &mut Bencher) {
   let mut v = StaticVec::<u8, 512>::new();
-  let cap = v.capacity();
-  b.iter(|| {
-    v.clear();
-    let constant = black_box(1);
-    v.extend((0..cap).map(move |_| constant));
-    v[511]
-  });
-  b.bytes = v.capacity() as u64;
-}
-
-#[bench]
-fn arrayvec_extend_with_constant(b: &mut Bencher) {
-  let mut v = ArrayVec::<[u8; 512]>::new();
   let cap = v.capacity();
   b.iter(|| {
     v.clear();
@@ -75,19 +49,6 @@ fn staticvec_extend_with_range(b: &mut Bencher) {
 }
 
 #[bench]
-fn arrayvec_extend_with_range(b: &mut Bencher) {
-  let mut v = ArrayVec::<[u16; 512]>::new();
-  let cap = v.capacity();
-  b.iter(|| {
-    v.clear();
-    let range = 0..cap;
-    v.extend(black_box(range).map(|x| x as u16));
-    v[511]
-  });
-  b.bytes = v.capacity() as u64;
-}
-
-#[bench]
 fn staticvec_extend_with_slice(b: &mut Bencher) {
   let mut v = StaticVec::<u8, 512>::new();
   let data = [1; 512];
@@ -101,33 +62,8 @@ fn staticvec_extend_with_slice(b: &mut Bencher) {
 }
 
 #[bench]
-fn arrayvec_extend_with_slice(b: &mut Bencher) {
-  let mut v = ArrayVec::<[u8; 512]>::new();
-  let data = [1; 512];
-  b.iter(|| {
-    v.clear();
-    let iter = data.iter().map(|&x| x);
-    black_box(v.extend(iter));
-    v[511]
-  });
-  b.bytes = v.capacity() as u64;
-}
-
-#[bench]
 fn staticvec_extend_with_write(b: &mut Bencher) {
   let mut v = StaticVec::<u8, 512>::new();
-  let data = [1; 512];
-  b.iter(|| {
-    v.clear();
-    v.write(&data[..]).ok();
-    black_box(v[511])
-  });
-  b.bytes = v.capacity() as u64;
-}
-
-#[bench]
-fn arrayvec_extend_with_write(b: &mut Bencher) {
-  let mut v = ArrayVec::<[u8; 512]>::new();
   let data = [1; 512];
   b.iter(|| {
     v.clear();
---
+++ benchmark/push_pop_suite.rs
@@ -0,0 +1,286 @@
+#![allow(incomplete_features)]
+#![allow(clippy::all)]
+#![feature(test)]
+#![feature(const_generics)]
+
+extern crate test;
+
+use test::{black_box, Bencher};
+
+use staticvec::{staticvec, StaticVec};
+
+#[bench]
+fn u32_255_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u32, 255>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..255 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u32_512_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u32, 512>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..512 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u32_1024_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u32, 1024>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..1024 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u32_2048_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u32, 2048>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..2048 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u32_4096_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u32, 4096>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..4096 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u32_8192_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u32, 8192>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..8192 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u64_255_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u64, 255>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..255 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u64_512_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u64, 512>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..512 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u64_1024_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u64, 1024>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..1024 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u64_2048_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u64, 2048>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..2048 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u64_4096_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u64, 4096>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..4096 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u64_8192_staticvec_push(b: &mut Bencher) {
+  let mut v = StaticVec::<u64, 8192>::new();
+  b.iter(|| {
+    v.clear();
+    for i in 0..8192 {
+      v.push(black_box(i));
+    }
+  });
+  b.bytes = v.capacity() as u64;
+}
+
+#[bench]
+fn u32_255_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u32; 255];
+    for _ in 0..255 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 255 as u64;
+}
+
+#[bench]
+fn u32_512_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u32; 512];
+    for _ in 0..512 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 512 as u64;
+}
+
+#[bench]
+fn u32_1024_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u32; 1024];
+    for _ in 0..1024 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 1024 as u64;
+}
+
+#[bench]
+fn u32_2048_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u32; 2048];
+    for _ in 0..2048 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 2048 as u64;
+}
+
+#[bench]
+fn u32_4096_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u32; 4096];
+    for _ in 0..4096 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 4096 as u64;
+}
+
+#[bench]
+fn u32_8192_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u32; 8192];
+    for _ in 0..8192 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 8192 as u64;
+}
+
+#[bench]
+fn u64_255_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u64; 255];
+    for _ in 0..255 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 255 as u64;
+}
+
+#[bench]
+fn u64_512_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u64; 512];
+    for _ in 0..512 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 512 as u64;
+}
+
+#[bench]
+fn u64_1024_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u64; 1024];
+    for _ in 0..1024 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 1024 as u64;
+}
+
+#[bench]
+fn u64_2048_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u64; 2048];
+    for _ in 0..2048 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 2048 as u64;
+}
+
+#[bench]
+fn u64_4096_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u64; 4096];
+    for _ in 0..4096 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 4096 as u64;
+}
+
+#[bench]
+fn u64_8192_staticvec_pop(b: &mut Bencher) {
+  b.iter(|| {
+    let mut v = staticvec![128u64; 8192];
+    for _ in 0..8192 {
+      black_box(v.pop().unwrap());
+    }
+  });
+  b.bytes = 8192 as u64;
+}
