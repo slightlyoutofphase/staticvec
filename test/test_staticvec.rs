aca40e92b6a584753f29c8173d7d1846b10bbc02
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2020-09-16 10:52:45 -0400

    See if this does anything

--- test/test_staticvec.rs
+++ test/test_staticvec.rs
@@ -1,7 +1,8 @@
 #![allow(clippy::all)]
 #![allow(dead_code)]
 #![allow(unused_imports)]
-#![feature(box_syntax, const_fn)]
+#![allow(incomplete_features)]
+#![feature(box_syntax, const_fn, const_generics, const_trait_impl)]
 
 // In case you're wondering, the instances of `#[cfg_attr(all(windows, miri), ignore)]` in this
 // file above the `#[should_panic]` tests are there simply because Miri only supports catching
