f88210fdd506da7a1550a8d8553dd1ca7f77d060
slightlyoutofphase <slightlyoutofphase@gmail.com>
2021-03-09 00:35:31 -0500

    I always forget something...

--- ci/run_linux_and_mac.sh
+++ ci/run_linux_and_mac.sh
@@ -6,8 +6,8 @@ case $(uname | tr '[:upper:]' '[:lower:]') in
     export APPVEYOR_OS_NAME=osx
     ;;
 esac
-if [ "$APPVEYOR_OS_NAME" = "linux" ]; then export PATH="/home/appveyor/.cargo/bin:$PATH"
-if [ "$APPVEYOR_OS_NAME" = "osx" ]; then export PATH="$HOME/.cargo/bin:$PATH"
+if [ "$APPVEYOR_OS_NAME" = "linux" ]; then export PATH="/home/appveyor/.cargo/bin:$PATH"; fi
+if [ "$APPVEYOR_OS_NAME" = "osx" ]; then export PATH="$HOME/.cargo/bin:$PATH"; fi
 if [ "$APPVEYOR_OS_NAME" = "linux" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri); fi
 if [ "$APPVEYOR_OS_NAME" = "osx" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-apple-darwin/miri); fi
 echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
