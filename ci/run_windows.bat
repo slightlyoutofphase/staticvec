97fd3dc7fd34d88abcedee1d59bcd1c6d687b20d
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2021-10-21 14:55:11 -0400

    Nearly there!

--- ci/run_windows.bat
+++ ci/run_windows.bat
@@ -1,4 +1,4 @@
 cargo clean
-cargo run --example main_demo && cargo run --example serde_support_demo && cargo run --example string_demo
+cargo run --example main_demo && cargo run --features="std serde_support serde_json_support" --example serde_support_demo && cargo run --example string_demo
 REM cargo test --features="std" --target=%TARGET%
 REM cargo test --no-default-features --target=%TARGET%
