9dbbf61bbed5c8f01cec74781ec64218e4db447b
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2021-10-21 14:09:43 -0400

    Temporarily adjust CI file to run demos

--- ci/run_windows.bat
+++ ci/run_windows.bat
@@ -1,3 +1,4 @@
 cargo clean
-cargo test --features="std" --target=%TARGET%
-cargo test --no-default-features --target=%TARGET%
+cargo run --example main_demo && cargo run --example serde_support_demo && cargo run --example string_demo
+REM cargo test --features="std" --target=%TARGET%
+REM cargo test --no-default-features --target=%TARGET%
