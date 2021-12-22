c107f75d0428459d77006b2efe0e703580afe037
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2021-12-22 15:47:27 -0500

    Try out alternate way of implementing `distance_between` per Ralf Jung's suggestion

--- src/utils.rs
+++ src/utils.rs
@@ -7,11 +7,11 @@ use crate::StaticVec;
 /// An internal function for calculating pointer offsets as usizes, while accounting
 /// directly for possible ZSTs. This is used specifically in the iterator implementations.
 #[inline(always)]
-pub(crate) /*const*/ fn distance_between<T>(dest: *const T, origin: *const T) -> usize {
+pub(crate) const fn distance_between<T>(dest: *const T, origin: *const T) -> usize {
+  // Safety: this function is used strictly with linear inputs
+  // where `dest` is known to come after `origin`.
   match size_of::<T>() {
-    0 => (dest as usize).wrapping_sub(origin as usize),
-    // Safety: this function is used strictly with linear inputs
-    // where dest is known to come after origin.
+    0 => unsafe { ptr_offset_from(dest as *const u8, origin as *const u8) as usize },
     _ => unsafe { ptr_offset_from(dest, origin) as usize },
   }
 }
