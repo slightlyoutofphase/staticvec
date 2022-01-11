a68bfe2e2d6da44e2ef3876dcf8a2384fb9899f4
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2022-01-11 01:16:44 -0500

    Woops

--- ci/run_linux_and_mac.sh
+++ ci/run_linux_and_mac.sh
@@ -11,8 +11,8 @@ if [ "$APPVEYOR_OS_NAME" = "osx" ]; then export PATH="$HOME/.cargo/bin:$PATH"; f
 if [ "$APPVEYOR_OS_NAME" = "linux" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri); fi
 if [ "$APPVEYOR_OS_NAME" = "osx" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-apple-darwin/miri); fi
 # echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
-# rustup set profile minimal
-# rustup default "$MIRI_NIGHTLY"
+rustup set profile minimal
+rustup default "$MIRI_NIGHTLY"
 # rustup component add miri
 # The `-Zmiri-disable-isolation` is so Miri can access the system clock
 # while calling `SystemTime::now()` in one of the tests.
