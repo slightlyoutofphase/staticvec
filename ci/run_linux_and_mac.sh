272c7491e7a5c587302b7e499dd88e7c38e11179
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2022-01-11 01:30:14 -0500

    Put CI file back to normal

--- ci/run_linux_and_mac.sh
+++ ci/run_linux_and_mac.sh
@@ -10,19 +10,21 @@ if [ "$APPVEYOR_OS_NAME" = "linux" ]; then export PATH="/home/appveyor/.cargo/bi
 if [ "$APPVEYOR_OS_NAME" = "osx" ]; then export PATH="$HOME/.cargo/bin:$PATH"; fi
 if [ "$APPVEYOR_OS_NAME" = "linux" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri); fi
 if [ "$APPVEYOR_OS_NAME" = "osx" ]; then MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-apple-darwin/miri); fi
-# echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
+echo "Installing latest nightly with Miri: $MIRI_NIGHTLY"
 rustup set profile minimal
 rustup default "$MIRI_NIGHTLY"
-# rustup component add miri
+rustup component add miri
 # The `-Zmiri-disable-isolation` is so Miri can access the system clock
 # while calling `SystemTime::now()` in one of the tests.
-# export MIRIFLAGS="-Zmiri-disable-isolation"
+export MIRIFLAGS="-Zmiri-disable-isolation"
 # We run the suite once under Miri with all functionality enabled, and then once
 # normally without the default features just to make sure `no_std` support has
 # not been broken.
-# cargo clean
-# cargo miri test --features="std"
-# cargo clean
-# cargo test --no-default-features
 cargo clean
-if [ "$APPVEYOR_OS_NAME" = "linux" ]; then cd ./fuzz && cargo install cargo-fuzz && cargo fuzz run ops; fi
+cargo miri test --features="std"
+cargo clean
+cargo test --no-default-features
+# Uncommenting the last two lines also runs the libfuzzer fuzz target, but it's more suited for running
+# locally than on CI currently due to the amount of time it takes to be effective
+# cargo clean
+# if [ "$APPVEYOR_OS_NAME" = "linux" ]; then cd ./fuzz && cargo install cargo-fuzz && cargo fuzz run ops; fi
