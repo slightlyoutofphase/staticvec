7b17ff1846bfb637b2937bf20eae2e7fc7b36791
SlightlyOutOfPhase <slightlyoutofphase@gmail.com>
2022-01-13 13:12:56 -0500

    Switch back to previous version of test to see if the new handling fixes the problem

--- test/test_staticvec.rs
+++ test/test_staticvec.rs
@@ -498,10 +498,7 @@ fn drain_iter() {
     ZST {},
     ZST {},
   ];
-  let mut v5_drain = v5.drain_iter(6..12);
-  assert_eq!(v5_drain.len(), 6);
-  while let Some(_x) = v5_drain.next() {}
-  drop(v5_drain);
+  assert_eq!(v5.drain_iter(6..12).len(), 6);
   assert_eq!(v5.len(), 10);
   assert_eq!(
     staticvec![1, 2, 3, 4]
